03a1eb8279e55d7d1882b062075cb0116a6ce51f
Will Shahda <will.shahda@gmail.com>
2017-08-15 03:05:26 -0400

    Initial commit

---
+++ gdax_trader.py
@@ -0,0 +1,129 @@
+import logging
+import os
+
+from requests.exceptions import ConnectionError
+
+import gdax
+
+from utils import connection_retry
+
+
+logger = logging.getLogger(__name__)
+
+
+class GDAXTrader:
+
+    # Environment variables required for authenticating with GDAX
+    GDAX_KEY_ENV = 'GDAX_KEY'
+    GDAX_SECRET_ENV = 'GDAX_SECRET'
+    GDAX_PASSPHRASE_ENV = 'GDAX_PASSPHRASE'
+    GDAX_API_URL_ENV = 'GDAX_API_URL'
+
+    # GDAX rate limit of 3 requests per second with a little extra padding
+    RATE_LIMIT = 1.0 / 3.0 + 0.5
+
+    # Maximum number of retry attempts after a connection error
+    MAX_RETRIES = 5
+
+    def __init__(self):
+        self.products = []
+        self.strategies = []
+        self.client = GDAXTrader._get_client()
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def add_strategy(self, strategy):
+        self.strategies.append(strategy)
+
+    def run(self):
+        """
+        Start the GDAX trading algorithm
+
+        Uses all products and strategies added to the `GDAXTrader`.
+        """
+
+        running = True
+
+        while running:
+            success = self._run_iteration()
+
+            if not success:
+                logger.warning('Data unavailable, iteration skipped...')
+
+    def _run_iteration(self):
+        """
+        Perform an iteration of the GDAX trading algorithm
+        """
+
+        # Retrieve all trading account data
+        try:
+            accounts = self._get_accounts()
+
+        # Skip iteration if account data is unavailable
+        except ConnectionError:
+            return False
+
+        tick_data = {}
+
+        # Get all product ticker data
+        for product in self.products:
+            try:
+                tick_data[product] = self._get_product_ticker(product)
+
+            # Skip iteration if ticker data is unavailable
+            except ConnectionError:
+                return False
+
+        # Update all strategies
+        for strategy in self.strategies:
+            strategy.next(tick_data, accounts)
+
+        return True
+
+    @classmethod
+    def _get_client(cls):
+        """
+        Get an authenticated GDAX client
+
+        :returns: an authenticated GDAX client
+        :raises KeyError: Error raised if environment variables are not set
+        """
+
+        try:
+            key = os.environ[GDAXTrader.GDAX_KEY_ENV]
+            secret = os.environ[GDAXTrader.GDAX_SECRET_ENV]
+            passphrase = os.environ[GDAXTrader.GDAX_PASSPHRASE_ENV]
+        except KeyError as error:
+            raise KeyError('Missing environment variable for GDAX: '.format(error))
+
+        try:
+            api_url = os.environ[GDAXTrader.GDAX_API_URL_ENV]
+        except KeyError:
+            client = gdax.AuthenticatedClient(key, secret, passphrase)
+        else:
+            client = gdax.AuthenticatedClient(key, secret, passphrase,
+                    api_url=api_url)
+
+        return client
+
+    @connection_retry(MAX_RETRIES, RATE_LIMIT)
+    def _get_product_ticker(self, product):
+        """
+        Get ticker data for a product
+
+        :param product: the GDAX product
+        :returns: ticker data
+        """
+
+        return self.client.get_product_ticker(product_id=product)
+
+    @connection_retry(MAX_RETRIES, RATE_LIMIT)
+    def _get_accounts(self):
+        """
+        Get accounts data
+
+        :returns: accounts data
+        """
+
+        return self.client.get_accounts()
---
+++ test_gdax_trader.py
@@ -0,0 +1,139 @@
+from requests.exceptions import ConnectionError
+import unittest
+from unittest.mock import patch, MagicMock
+
+from gdax_trader import GDAXTrader
+
+
+class GDAXTraderTestCase(unittest.TestCase):
+    """
+    Test :class:`GDAXTrader`
+
+    Methods:
+        - :meth:`GDAXTrader._run_iteration`
+        - :meth:`GDAXTrader._get_client`
+        - :meth:`GDAXTrader._get_product_ticker`
+    """
+
+    @patch('gdax_trader.GDAXTrader._get_client')
+    def test__run_iteration_success(self, client):
+        """
+        Test :meth:`GDAXTrader._run_iteration`
+
+        Assert ticker data is retrieved for every product, every strategy
+        is iterated, and `True` is returned.
+        """
+
+        trader = GDAXTrader()
+        trader.add_product('BTC-USD')
+
+        strategy = MagicMock()
+        trader.add_strategy(strategy)
+
+        trader._get_product_ticker = MagicMock()
+
+        result = trader._run_iteration()
+
+        self.assertEqual(trader._get_product_ticker.call_count, 1)
+        self.assertEqual(strategy.next.call_count, 1)
+        self.assertTrue(result)
+
+    @patch('gdax_trader.GDAXTrader._get_client')
+    def test__run_iteration_with_account_error(self, client):
+        """
+        Test :meth:`GDAXTrader._run_iteration`
+
+        Assert method returns `False` and that strategies are not updated.
+        """
+
+        trader = GDAXTrader()
+        trader.add_product('BTC-USD')
+
+        strategy = MagicMock()
+        trader.add_strategy(strategy)
+
+        trader._get_accounts = MagicMock(side_effect=ConnectionError) 
+        trader._get_product_ticker = MagicMock()
+
+        result = trader._run_iteration()
+
+        self.assertEqual(trader._get_product_ticker.call_count, 0)
+        self.assertEqual(strategy.next.call_count, 0)
+        self.assertFalse(result)
+
+    @patch('gdax_trader.GDAXTrader._get_client')
+    def test__run_iteration_with_ticker_error(self, client):
+        """
+        Test :meth:`GDAXTrader._run_iteration`
+
+        Assert method returns `False` and that strategies are not updated.
+        """
+
+        trader = GDAXTrader()
+        trader.add_product('BTC-USD')
+
+        strategy = MagicMock()
+        trader.add_strategy(strategy)
+
+        trader._get_accounts = MagicMock() 
+        trader._get_product_ticker = MagicMock(side_effect=ConnectionError)
+
+        result = trader._run_iteration()
+
+        self.assertEqual(strategy.next.call_count, 0)
+        self.assertFalse(result)
+
+    def test__get_client_with_env_and_api_url(self):
+        """
+        Test :meth:`GDAXTrader._get_client`
+
+        Assert a client is created with a key, secret, passphrase, and API url.
+        """
+
+        environ = {
+            GDAXTrader.GDAX_KEY_ENV: '1',
+            GDAXTrader.GDAX_SECRET_ENV: '1',
+            GDAXTrader.GDAX_PASSPHRASE_ENV: '1',
+            GDAXTrader.GDAX_API_URL_ENV: '1',
+        }
+
+        with patch.dict('os.environ', environ, True):
+            with patch('gdax.AuthenticatedClient') as gdax:
+                client = GDAXTrader._get_client()
+
+                gdax.assert_called_with(environ[GDAXTrader.GDAX_KEY_ENV],
+                        environ[GDAXTrader.GDAX_KEY_ENV],
+                        environ[GDAXTrader.GDAX_PASSPHRASE_ENV],
+                        api_url=environ[GDAXTrader.GDAX_API_URL_ENV])
+
+    def test__get_client_with_env(self):
+        """
+        Test :meth:`GDAXTrader._get_client`
+
+        Assert a client is created with a key, secret, and passphrase.
+        """
+
+        environ = {
+            GDAXTrader.GDAX_KEY_ENV: '1',
+            GDAXTrader.GDAX_SECRET_ENV: '1',
+            GDAXTrader.GDAX_PASSPHRASE_ENV: '1',
+        }
+
+        with patch.dict('os.environ', environ, True):
+            with patch('gdax.AuthenticatedClient') as gdax:
+                client = GDAXTrader._get_client()
+
+                gdax.assert_called_with(environ[GDAXTrader.GDAX_KEY_ENV],
+                        environ[GDAXTrader.GDAX_KEY_ENV],
+                        environ[GDAXTrader.GDAX_PASSPHRASE_ENV])
+
+    def test__get_client_without_env(self):
+        """
+        Test :meth:`GDAXTrader._get_client`
+
+        Assert an exception is raised that indicates missing environment
+        variables.
+        """
+
+        with patch.dict('os.environ', {}, True), self.assertRaises(KeyError):
+            client = GDAXTrader._get_client()
